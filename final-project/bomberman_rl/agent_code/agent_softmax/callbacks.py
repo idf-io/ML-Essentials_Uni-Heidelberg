e46ac69105f8f809bd4e610867e5024ff5a53084
sevdeyanik <sevdeyanik@hotmail.com>
2023-08-31 15:02:58 +0200

    add softmax exploration strategy under agent_softmax

---
+++ final-project/bomberman_rl/agent_code/agent_softmax/callbacks.py
@@ -0,0 +1,114 @@
+import os
+import pickle
+import numpy as np
+
+from settings import BOMB_POWER, COLS
+
+# List of available actions
+ACTIONS = ['UP', 'RIGHT', 'DOWN', 'LEFT', 'WAIT']
+
+def setup(self):
+    # Exploration parameters
+    self.epsilon = 1.0  # Initial exploration rate
+    self.learning_rate = 0.1  # Learning rate for Q-learning
+    self.discount_factor = 0.9  # Discount factor for future rewards
+
+    # Check if Q-table file exists, and load it if available
+    if self.train or not os.path.isfile("my-saved-qtable.pkl"):
+        self.logger.info("Setting up Q-table from scratch.")
+        self.q_table = {}  # Initialize Q-table
+    else:
+        self.logger.info("Loading Q-table from saved state.")
+        with open("my-saved-qtable.pkl", "rb") as file:
+            self.q_table = pickle.load(file)
+
+# Softmax function to convert Q-values into probabilities
+def softmax(x):
+    e_x = np.exp(x - np.max(x))
+    return e_x / e_x.sum()
+
+# Select an action using epsilon-greedy or softmax exploration
+def act(self, game_state: dict) -> str:
+    if game_state is None:
+        return np.random.choice(ACTIONS)
+
+    state = state_to_features(game_state)
+
+    if self.train:
+        q_values = self.q_table.get(tuple(state), {a: 0.0 for a in ACTIONS})
+        probabilities = softmax(list(q_values.values()))
+        chosen_action = np.random.choice(ACTIONS, p=probabilities)
+        return chosen_action
+    else:
+        # In testing mode, choose the action with the highest Q-value (exploitation)
+        q_values = self.q_table.get(tuple(state), {a: 0.0 for a in ACTIONS})
+        max_q = max(q_values.values())
+        best_actions = [a for a, q in q_values.items() if q == max_q]
+        # If there are multiple actions with the same max Q-value, select one randomly
+        chosen_action = np.random.choice(best_actions)
+        return chosen_action
+
+# Convert game state to feature vector
+def state_to_features(game_state: dict) -> np.array:
+    if game_state is None:
+        return None
+
+    field = game_state["field"]
+    self_position = game_state["self"][3]
+    new_field = field2bomb(game_state)
+    new_field = field2coin(game_state, new_field)
+
+    features = []
+    for x in range(new_field.shape[0]):
+        for y in range(new_field.shape[1]):
+            cell = new_field[x, y]
+            if cell == 2:
+                features.extend([1, 0, 0])
+            elif cell == 1:
+                features.extend([0, 1, 0])
+            elif cell == 0:
+                features.extend([0, 1, 1])
+            elif cell == -1:
+                features.extend([1, 1, 0])
+            elif cell == 3:
+                features.extend([0, 0, 1])
+            else:
+                features.extend([1, 1, 1])
+
+    features.append(self_position[0])
+    features.append(self_position[1])
+    return np.array(features)
+
+# Update the game field to include bomb explosion tiles
+def field2bomb(game_state: dict, power=BOMB_POWER, board_size=COLS):
+
+    """
+    Convert the field array to include tiles where the explosion takes place next move.
+    """
+
+    bomb_mask = np.zeros([board_size, board_size])
+    #0 dimension indicates the position of bomb,1 dimension indicates the time before explosion (0 right before explosion)
+    for bomb in game_state["bombs"]:
+        if bomb[1] == 0:
+            bomb_mask[max(bomb[0][0]-power,0):min(bomb[0][0]+power+1,board_size),bomb[0][1]] = 1
+            bomb_mask[bomb[0][0]][max(bomb[0][1]-power,0):min(bomb[0][1]+power+1,board_size)] = 1
+
+    new_field = (game_state["field"] == 0) & np.array(bomb_mask, dtype=bool)
+    new_field = np.where(new_field, 2, game_state["field"])
+
+    return new_field
+
+# Update the game field to include coin locations
+def field2coin(game_state: dict, new_field):
+    """
+    :param game_state: current state of the game
+    :param new_field: field with walls, crates and bombs
+    :return: new_field with coin locations
+    """
+    coins = game_state["coins"]
+
+    for coin in coins:
+        if new_field[coin[0]][coin[1]] == 0 :
+            new_field[coin[0]][coin[1]] = 3
+
+    return new_field
--- final-project/bomberman_rl/agent_code/agent_softmax/train.py
+++ final-project/bomberman_rl/agent_code/agent_softmax/train.py
@@ -5,27 +5,32 @@ import events as e
 from .callbacks import state_to_features
 from typing import List
 
-
+# Define a named tuple to represent a transition
 Transition = namedtuple('Transition', ('state', 'action', 'next_state', 'reward'))
 
+# Hyperparameters
 TRANSITION_HISTORY_SIZE = 5
 RECORD_ENEMY_TRANSITIONS = 1.0
 PLACEHOLDER_EVENT = "PLACEHOLDER"
-
 ACTIONS = ['UP', 'RIGHT', 'DOWN', 'LEFT', 'WAIT', 'BOMB']
 
+
 def setup_training(self):
-    self.transitions = deque(maxlen=TRANSITION_HISTORY_SIZE) #maintain a replay buffer (a deque of transitions) to store experiences.
-    self.learning_rate = 0.01
+    # Initialize replay buffer to store experiences
+    self.transitions = deque(maxlen=TRANSITION_HISTORY_SIZE)
+
+    # Q-learning hyperparameters
+    self.learning_rate = 0.1
     self.discount_factor = 0.95
-    self.epsilon = 1.0
-    self.epsilon_decay = 0.9
-    self.min_epsilon = 0.05
-    self.gamma = 0.95
-    self.alpha = 0.1
+    self.epsilon = 1.0  # Exploration factor
+    self.epsilon_decay = 0.9  # Decay rate for exploration
+    self.min_epsilon = 0.05  # Minimum exploration factor
+    self.gamma = 0.95  # Discount factor for rewards in the Bellman equation
+    self.alpha = 0.1  # Learning rate for Q-value updates
+
 
 def update_q_values(self, gamma):
-    #sample batches from the replay buffer and update Q-values based on the Bellman equation.
+    # Sample batches from the replay buffer and update Q-values based on the Bellman equation.
     while self.transitions:
         transition = self.transitions.popleft()
         state = transition.state
@@ -50,18 +55,21 @@ def update_q_values(self, gamma):
             self.q_table[tuple(state)] = {}
         self.q_table[tuple(state)][action] = updated_q_value
 
+    # Save updated Q-table
     with open("my-saved-qtable.pkl", "wb") as file:
         pickle.dump(self.q_table, file)
+
 
 def game_events_occurred(self, old_game_state: dict, self_action: str, new_game_state: dict, events: List[str]):
     self.logger.debug(f'Encountered game event(s) {", ".join(map(repr, events))} in step {new_game_state["step"]}')
     # Calculate reward based on events
     reward = reward_from_events(self, events)
-    # add transitions to the replay buffer (store the state, action, next state, and reward)
+    # Add transitions to the replay buffer (store the state, action, next state, and reward)
     self.transitions.append(
         Transition(state_to_features(old_game_state), self_action, state_to_features(new_game_state), reward))
-    # Gradually decrease epsilon
+    # Gradually decrease epsilon for exploration
     self.epsilon = max(self.epsilon * self.epsilon_decay, self.min_epsilon)
+
 
 def end_of_round(self, last_game_state: dict, last_action: str, events: List[str]):
     self.logger.debug(f'Encountered event(s) {", ".join(map(repr, events))} in final step')
@@ -69,7 +77,9 @@ def end_of_round(self, last_game_state: dict, last_action: str, events: List[str
     reward = reward_from_events(self, events)
     self.transitions.append(Transition(state_to_features(last_game_state), last_action, None, reward))
 
+
 def reward_from_events(self, events: List[str]) -> float:
+    # Define rewards for different events
     game_rewards = {
         e.COIN_COLLECTED: 5.0,
         e.KILLED_OPPONENT: 3.0,
@@ -78,7 +88,8 @@ def reward_from_events(self, events: List[str]) -> float:
         e.COIN_FOUND: 1.0,
         e.GOT_KILLED: -2.0,
         e.CRATE_DESTROYED: 1.0,
-        PLACEHOLDER_EVENT: -0.1
+        PLACEHOLDER_EVENT: -0.1,
+        e.INVALID_ACTION: -0.1
     }
     reward_sum = 0.0
     for event in events:
